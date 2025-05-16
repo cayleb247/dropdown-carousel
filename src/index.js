eab365df308f0b3e5023efb6764732f99ac736d1
Caleb Wang <156046197+cayleb247@users.noreply.github.com>
2025-05-16 10:34:42 -0400

    Complete dropdown MVP

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import "./styles.css";
 
+const dropdownContainer = document.querySelector(".dropdown-container");
+const dropdownButton = document.querySelector(".dropdown-button");
+const dropdownContent = document.querySelector(".dropdown-content");
+
+dropdownContainer.addEventListener('mouseenter', () => {
+    dropdownContent.style.visibility = "visible";
+    dropdownButton.classList.add('dropdown-button-hover');
+})
+
+dropdownContainer.addEventListener('mouseleave', () => {
+    dropdownContent.style.visibility = "hidden";
+    dropdownButton.classList.remove('dropdown-button-hover');
+})
