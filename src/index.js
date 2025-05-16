b1288e7c9058d91f87938dc52975099785a2f318
Caleb Wang <156046197+cayleb247@users.noreply.github.com>
2025-05-16 11:44:53 -0400

    Fix dropdown behavior
    Iterates through all dropdowns and adds functionality to all of them

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import "./styles.css";
 
-const dropdownContainer = document.querySelector(".dropdown-container");
-const dropdownButton = document.querySelector(".dropdown-button");
-const dropdownContent = document.querySelector(".dropdown-content");
+const dropdownContainers = document.querySelectorAll(".dropdown-container");
+const dropdownButtons = document.querySelectorAll(".dropdown-button");
+const dropdownContents = document.querySelectorAll(".dropdown-content");
 
-dropdownContainer.addEventListener('mouseenter', () => {
-    dropdownContent.style.visibility = "visible";
-    dropdownButton.classList.add('dropdown-button-hover');
-})
+for (let i=0; i<dropdownContainers.length;i++) {
+    dropdownContainers[i].addEventListener('mouseenter', () => {
+        dropdownContents[i].style.visibility = "visible";
+        dropdownButtons[i].classList.add('dropdown-button-hover');
+    })
+    dropdownContainers[i].addEventListener('mouseleave', () => {
+        dropdownContents[i].style.visibility = "hidden";
+        dropdownButtons[i].classList.remove('dropdown-button-hover');
+    })
+}
 
-dropdownContainer.addEventListener('mouseleave', () => {
-    dropdownContent.style.visibility = "hidden";
-    dropdownButton.classList.remove('dropdown-button-hover');
-})
